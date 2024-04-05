cb93f64ce784a03067059dee60a0845a18b4eb25
sunilpanakal <sunilpanakal@gmail.com>
2024-04-05 10:05:07 +0530

    updated one again

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -6,6 +6,7 @@ import { useTitle } from "../hooks/useTitle";
 export const MovieDetail = (title) => {
   const params = useParams();
   const [movie, setMovie] = useState({});
+  //eslint-disable-next-line
   const pageTitle = useTitle(movie.title);
   const image = movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : Backup;
 
